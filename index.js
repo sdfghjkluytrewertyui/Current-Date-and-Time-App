d9525bd0a4b62e201ec7178b82c648a8fb111b6d
dfghjkcvbnmnbvcx <jaadonaoisv@hotmail.com>
2024-03-16 17:03:31 +0700

    init project

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+function getCurrentDateTime() {
+    const currentDate = new Date();
+    return currentDate.toLocaleString();
+  }
+  
+  console.log('Current date and time:', getCurrentDateTime());
+  
